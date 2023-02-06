c50c3e4ebbd300c7f95c85dfd1904b3cae6e9b05
Krishna Kanta Pal <85862415+kkpal02@users.noreply.github.com>
2023-02-06 22:00:58 +0530

    Switch_Break_Continue
    This lecture doesnot include much code only small code is there, switch case only having small code that i upload it.

---
+++ lecture5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int val;
+    cout<<"Enter the value=";
+    cin>>val;
+
+    // switch(val){
+    //     case 1: cout<<"hii";
+    //     break;
+    //     case 2: cout<<"boy";
+    //     break;
+    //     case 3: cout<<"girl";
+    //     break;
+    //     case 4: cout<<"transgender";
+    //     break;
+    //     case 5: cout<<"not to say";
+    //     break;
+    //     case 6: cout<<"ok thanks";
+    //     break;
+    //     default: cout<<"dead";
+    // }
+
+
+    
+}
